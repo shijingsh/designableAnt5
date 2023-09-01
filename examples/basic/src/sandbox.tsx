32482daac950c7a0dc81df2ae0bca92d883620b0
liukefu2050 <liukefu2050@sina.com>
2023-09-01 10:25:33 +0800

    antd5  1.0.0-beta.47

--- examples/basic/src/sandbox.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { Content } from './content'
-import { renderSandboxContent } from '@liukefu/designable-react-sandbox'
-import './theme.less'
-
-renderSandboxContent(() => {
-  return <Content />
-})
--- examples/sandbox/src/sandbox.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import { Content } from './content'
-import { renderSandboxContent } from '@liukefu/designable-react-sandbox'
-
-renderSandboxContent(() => {
-  return <Content />
-})
